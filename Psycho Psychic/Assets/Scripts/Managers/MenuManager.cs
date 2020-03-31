ffbde03f6015ef6c393f0418f16c042c7d6ea6c1
Sono528 <Sono528@users.noreply.github.com>
2020-03-31 13:04:43 -0400

    MainMenu
    Added Main Menu script
    Added Main Menu Prefab
    Added Main Menu Scene
    
    -Issue with script not attaching to prefab

---
+++ Psycho Psychic/Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenu;
+    [SerializeField] private GameObject levelSelect;
+    [SerializeField] private GameObject settings;
+    [SerializeField] private GameObject credits;
+    [SerializeField] private GameObject quitPrompt;
+
+    public void GotoLevel1()
+    {
+        //SceneManager.LoadScene("Test");
+    }
+    public void GotoLevel2()
+    {
+        //SceneManager.LoadScene("Popup Text Test");
+    }
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+    public void OpenLevelSelect()
+    {
+        mainMenu.SetActive(false);
+        levelSelect.SetActive(true);
+        settings.SetActive(false);
+        credits.SetActive(false);
+        quitPrompt.SetActive(false);
+    }
+    public void OpenSettings()
+    {
+        mainMenu.SetActive(false);
+        levelSelect.SetActive(false);
+        settings.SetActive(true);
+        credits.SetActive(false);
+        quitPrompt.SetActive(false);
+    }
+    public void OpenCredits()
+    {
+        mainMenu.SetActive(false);
+        levelSelect.SetActive(false);
+        settings.SetActive(false);
+        credits.SetActive(true);
+        quitPrompt.SetActive(false);
+    }
+    public void OpenQuitPrompt()
+    {
+        mainMenu.SetActive(false);
+        levelSelect.SetActive(false);
+        settings.SetActive(false);
+        credits.SetActive(false);
+        quitPrompt.SetActive(true);
+    }
+    public void ClosePrompts()
+    {
+        mainMenu.SetActive(true);
+        levelSelect.SetActive(false);
+        settings.SetActive(false);
+        credits.SetActive(false);
+        quitPrompt.SetActive(false);
+    }
+}
